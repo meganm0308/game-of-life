7bd6b807ced957a1b9742ea0ec146c5371b2c70a
meganm0308 <menghanma@gmail.com>
2020-10-06 11:30:48 -0700

    prints all generations

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class Main {
     private final static Scanner scanner = new Scanner(System.in);
 
-    private static String[][] firstState (int matrixSize, long seed) {
+    private static String[][] firstState (int matrixSize) {
         String[][] state = new String[matrixSize][matrixSize];
 
-        final Random random = new Random(seed);
+        final Random random = new Random();
         for (int i = 0; i < matrixSize; i++) {
             for (int j = 0; j < matrixSize; j++) {
                 state[i][j] = random.nextBoolean()? "O" : " ";
@@ -27,19 +27,35 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int matrixSize = scanner.nextInt();
-        long seed = scanner.nextInt();
-        int generations = scanner.nextInt();
-        String[][] state = firstState(matrixSize, seed);
+        int generations = 10;
 
-        if (generations >= 1) {
-            for (int i = 0; i < generations; i++) {
-                UniverseStates states = new UniverseStates(state);
-                states.setCurrentState();
-                state = states.getCurrentState();
+        // 1st generation
+        String[][] state = firstState(matrixSize);
+
+        for (int i = 1; i <= generations; i++) {
+            // print the ith generation
+            int countAlive = 0;
+            for (int j = 0; j < state.length; j++) {
+                for (int k = 0; k < state.length; k++) {
+                    if (state[j][k].equals("O")) {
+                        countAlive += 1;
+                    }
+                }
             }
+            System.out.println("Generation #" + i);
+            System.out.println("Alive: " + countAlive);
+            printState(state);
+
+            // get the next generation
+            UniverseStates states = new UniverseStates(state);
+            states.setCurrentState();
+            state = states.getCurrentState();
+
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            Thread.sleep(100);
         }
-        printState(state);
     }
 }
